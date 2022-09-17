02064adf165923cc48e9c000ed6d114d6f9afe2f
Ali Aliev <ali.aliev.msk>
2022-09-17 21:23:16 +0300

    first

---
+++ map.js
@@ -0,0 +1,37 @@
+//
+// const map = document.getElementById("#map");
+//
+//
+// window.addEventListener("load", () =>{{
+//     const map_canvas = document.getElementById("map-canvas");
+//     const ctx =  map_canvas.getContext("2d")
+//
+//
+//     // ctx.fillStyle = 'blue';
+//     // ctx.fillRect(50, 10, 1, 1);
+//     // ctx.strokeStyle = 'blue'
+//     // ctx.beginPath()
+//     // ctx.moveTo(50, 50)
+//     // ctx.lineTo(120, 180)
+//     // ctx.stroke();
+//     // DrawBranches([51, 51], [50, 50], ctx)
+//
+// }});
+//
+// async function DrawBranches(target, from, ctx){
+//     ctx.strokeStyle = 'green'
+//     var x = target[0]
+//     var y = target[1]
+//     while(from !== target){
+//         ctx.moveTo(x, y)
+//         x +=  1
+//         // x += (Math.random() * 5)
+//         // y += (Math.random() * 5)
+//         y +=  1
+//         ctx.lineTo(x, y)
+//         ctx.stroke();
+//     }
+//
+//
+//
+// }
